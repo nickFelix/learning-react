018b93ca154fcc7631cb609748510ea279b7a734
Nicolas Felix <nicolas.felixa@gmail.com>
2017-06-01 22:28:16 -0300

    double component

--- exercicios_react/ex/component.jsx
+++ exercicios_react/ex/component.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 
-export default (props) => (
-  <h1>{props.value}</h1>
+export default props => (
+  <h1>Primeiro Componete</h1>
 )
+
+
+export const Segundo = props => (
+  <h1>Segundo Componente</h1>
+)
+
+// export { Primeiro, Segundo }
