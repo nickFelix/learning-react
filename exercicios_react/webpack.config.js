1cd5a0f9a2606913e6cc496d4576dd6a0a1fe88e
Nicolas Felix <nicolas.felixa@gmail.com>
2017-06-01 21:46:58 -0300

    initial config

---
+++ exercicios_react/webpack.config.js
@@ -0,0 +1,16 @@
+const webpack = require('webpack')
+
+module.exports = {
+  entry: './ex/index.js',
+  output: {
+    path: __dirname + '/public',
+    filename: './bundle.js'
+  },
+  devServer: {
+    port: 8080,
+    contentBase: './public'
+  },
+  module: {
+
+  }
+}
