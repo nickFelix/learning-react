ca83b2db4d746f697d0fb73943ddcfe05cb28535
Nicolas Felix <nicolas.felixa@gmail.com>
2017-06-20 18:19:55 -0300

    configuração todo-app backend

---
+++ todo-app/backend/src/config/server.js
@@ -0,0 +1,11 @@
+const port = 3003
+
+const bodyParser = require('body-parser')
+const express = require('express')
+const server = express()
+
+server.use(bodyParser.urlencoded({extend: true}))
+
+server.listen(port, function(){
+  console.log(`BACKEND is runnig on port ${port}.`)
+})
