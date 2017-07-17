c4f703ff8613c611a58e5f30f31b4551047b0240
nickFelix <nicolas.felixa@gmail.com>
2017-07-16 21:25:47 -0300

    Some alterations

---
+++ todo-app/frontend/src/about/about.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+  <div>
+    <PageHeader name='Sobre' small='nós' ></PageHeader>
+
+    <h2>Nóssa historia</h2>
+    <p>
+      Ainda assim, existem dúvidas a respeito de como o fenômeno da Internet cumpre um
+      papel essencial na formulação dos relacionamentos verticais entre as hierarquias.
+      É importante questionar o quanto o desafiador cenário globalizado auxilia a preparação
+      e a composição do processo de comunicação como um todo. Todavia, a crescente influência
+      da mídia estimula a padronização dos conhecimentos estratégicos para atingir a
+      excelência. No entanto, não podemos esquecer que a competitividade nas transações
+      comerciais nos obriga à análise das diretrizes de desenvolvimento para o futuro.
+     </p>
+    <h2>Missão e Visão</h2>
+    <p>
+      A certificação de metodologias que nos auxiliam a lidar com
+      a necessidade de renovação processual garante a contribuição de
+      um grupo importante na determinação de todos os recursos funcionais envolvidos.
+      Por conseguinte, o entendimento das metas propostas acarreta um
+      processo de reformulação e modernização do sistema de formação de quadros que
+      corresponde às necessidades. Do mesmo modo, o surgimento do comércio virtual pode nos
+      levar a considerar a reestruturação do fluxo de informações.
+     </p>
+  </div>
+)
